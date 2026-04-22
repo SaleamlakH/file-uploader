223f58e27437c23eb91b5159d246c5eef90196d2
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-22 16:52:22 +0300

    chore(types): add FolderCreateNew type

--- api/src/types/db.d.ts
+++ api/src/types/db.d.ts
@@ -1,5 +1,6 @@
 import type { Folders } from '../../generated/prisma/client';
 
 // folders table types
+export type FolderCreateNew = Pick<Folders, 'folder' | 'ownerId'>;
 export type FolderFetchData = Pick<Folders, 'id' | 'ownerId'>;
 export type FolderUpdate = Pick<Folders, 'id' | 'ownerId' | 'folder'>;
