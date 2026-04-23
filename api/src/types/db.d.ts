240158b2a47a55dd52c3fcc67d85ca389d47b633
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-23 13:51:25 +0300

    chore(types): add FileUpload type

--- api/src/types/db.d.ts
+++ api/src/types/db.d.ts
@@ -1,6 +1,9 @@
-import type { Folders } from '../../generated/prisma/client';
+import type { Files, Folders } from '../../generated/prisma/client';
 
 // folders table types
 export type FolderCreateNew = Pick<Folders, 'folder' | 'ownerId'>;
 export type FolderFetchData = Pick<Folders, 'id' | 'ownerId'>;
 export type FolderUpdate = Pick<Folders, 'id' | 'ownerId' | 'folder'>;
+
+// files table types
+export type FileUpload = Omit<Files, 'id' | 'uploadedAt' | 'folderId'>;
