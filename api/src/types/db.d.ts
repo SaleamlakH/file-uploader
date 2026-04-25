af7006e74e13b22d102b2a9f07fc7ac9bc4c9928
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-25 10:20:06 +0300

    chore(types): add FolderGetParameter type used for fetching folder data

--- api/src/types/db.d.ts
+++ api/src/types/db.d.ts
@@ -4,6 +4,11 @@ import type { Files, Folders } from '../../generated/prisma/client';
 export type FolderCreateNew = Pick<Folders, 'folder' | 'ownerId'>;
 export type FolderFetchData = Pick<Folders, 'id' | 'ownerId'>;
 export type FolderUpdate = Pick<Folders, 'id' | 'ownerId' | 'folder'>;
+export type FolderGetParameters = {
+  folderId: Folders['id'];
+  ownerId: Folders['ownerId'];
+  includeFiles: boolean;
+};
 
 // files table types
 export type FileUpload = Omit<Files, 'id' | 'uploadedAt' | 'folderId'>;
