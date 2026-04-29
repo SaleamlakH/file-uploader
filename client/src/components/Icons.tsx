e09c18975c8254270990780749d470d67e1ccb50
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-29 10:46:17 +0300

    feat(icons): add Caution and TrashBin icons

--- client/src/components/Icons.tsx
+++ client/src/components/Icons.tsx
@@ -170,3 +170,28 @@ export const MenuDots = () => {
     </svg>
   );
 };
+
+export const Caution = () => {
+  return (
+    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
+      <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M7 5v3" />
+        <circle cx="7" cy="11" r=".5" />
+        <path d="M7.89 1.05a1 1 0 0 0-1.78 0l-5.5 11a1 1 0 0 0 .89 1.45h11a1 1 0 0 0 .89-1.45Z" />
+      </g>
+    </svg>
+  );
+};
+
+export const TrashBin = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 16 16">
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M9 2H7a.5.5 0 0 0-.5.5V3h3v-.5A.5.5 0 0 0 9 2m2 1v-.5a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2V3H2.251a.75.75 0 0 0 0 1.5h.312l.317 7.625A3 3 0 0 0 5.878 15h4.245a3 3 0 0 0 2.997-2.875l.318-7.625h.312a.75.75 0 0 0 0-1.5zm.936 1.5H4.064l.315 7.562A1.5 1.5 0 0 0 5.878 13.5h4.245a1.5 1.5 0 0 0 1.498-1.438zm-6.186 2v5a.75.75 0 0 0 1.5 0v-5a.75.75 0 0 0-1.5 0m3.75-.75a.75.75 0 0 1 .75.75v5a.75.75 0 0 1-1.5 0v-5a.75.75 0 0 1 .75-.75"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
