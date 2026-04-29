a2c93d0280cdea08e77246299498b5398c8d5db0
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-29 18:51:21 +0300

    feat(folder-page): add table to list files

--- client/src/pages/folder-view/FolderView.tsx
+++ client/src/pages/folder-view/FolderView.tsx
@@ -1,13 +1,20 @@
 import Action from '../../components/Action/Action';
-import { Upload } from '../../components/Icons';
+import { Download, Upload } from '../../components/Icons';
 import style from './folder-view.module.css';
 import ShareForm from '../../components/share-form/ShareForm';
 import EditForm from '../../components/edit-form/EditForm';
 import DeleteFolder from '../../components/folder-delete/FolderDelete';
 import FolderActionMenu from '../../components/menu/FolderActionMenu';
 import UploadFileForm from '../../components/upload-file-form/UploadFileForm';
+import { useState } from 'react';
 
 export default function FolderView() {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const formatSize = (size: number) => {
+    if (typeof size !== 'number' || isNaN(size)) return '-';
+    return (size / 1024 / 1024).toFixed(2) + ' MB';
+  };
+
   return (
     <>
       <header className={style.header}>
@@ -36,8 +43,46 @@ export default function FolderView() {
         </div>
       </header>
 
+      {/* File table */}
+      <div className={style.wrapper}>
+        <table className={style.table}>
+          <thead>
+            <tr className={style.headRow}>
+              <th className={style.th}>Name</th>
+              <th className={style.th}>Size</th>
+              <th className={style.th}>Uploaded</th>
+              <th className={style.th}></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {[{ id: 1, name: 'File name', size: 1125434, uploaded: '2026-4-5' }].map((file, i) => (
+              <tr
+                key={file.id ?? file?.name ?? i}
+                className={`${style.row} ${hoveredIndex === i ? style.rowHover : ''}`}
+                onMouseLeave={() => setHoveredIndex(null)}
+              >
+                <td className={style.tdName}>
+                  <span className={style.nameText}>{file.name}</span>
+                </td>
+
+                <td className={style.td}>{formatSize(file.size)}</td>
+
+                <td className={style.td}>{file.uploaded}</td>
+
+                <td className={style.actionsCell}>
+                  <Action as="link" to="#" className={style.downloadBtn}>
+                    <Download />
+                  </Action>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
       {/* upload files */}
-      <UploadFileForm id="upload-file" />
+      <UploadFileForm />
 
       {/* share-folder form */}
       <ShareForm id="share-folder" />
