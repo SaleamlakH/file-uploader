48a254a3ffab55833f84eb4d459b11921054b68f
SaleamlakH <saleamlak.7h@gmail.com>
2026-05-01 12:33:25 +0300

    feat(api): add API type definitions

---
+++ client/src/api/types/api.d.ts
@@ -0,0 +1,42 @@
+export interface ApiSuccessResponse<T> {
+  data: T;
+  message?: string;
+}
+
+export interface ApiErrorResponse {
+  error: {
+    message: string;
+    errors?: Record<string, Record<string, string>>;
+  };
+}
+
+export interface User {
+  id?: string;
+  email: string;
+  createdAt: Date;
+}
+
+export interface Folder {
+  id: string;
+  name: string;
+  createdAt: string;
+}
+
+export interface File {
+  id: string;
+  filename: string;
+  size: number;
+  type: string;
+  uploadedAt: Date;
+  folderId: string;
+}
+
+export interface ShareLink {
+  path: string;
+  expiresAt: Date;
+}
+
+export interface SharedFolder extends Folder {
+  id: never;
+  files: File[];
+}
