524e2b8f8369b889c4dff889fadc26d8c5b37a3e
SaleamlakH <saleamlak.7h@gmail.com>
2026-05-01 12:34:21 +0300

    feat(api): add ApiError class

---
+++ client/src/api/error.ts
@@ -0,0 +1,12 @@
+import type { ApiErrorResponse } from './types/api';
+
+export class ApiError extends Error {
+  status;
+  errors;
+
+  constructor(status: number, message: string, errors: ApiErrorResponse) {
+    super(message);
+    this.status = status;
+    this.errors = errors;
+  }
+}
